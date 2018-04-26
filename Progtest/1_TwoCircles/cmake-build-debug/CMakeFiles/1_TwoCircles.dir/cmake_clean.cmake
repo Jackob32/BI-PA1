287adc389259de205e3b15660e8c47aee48eb166
Jackob32 <Jakub.trhlik@gmail.com>
2018-04-26 20:15:12 +0200

    update 3

---
+++ Progtest/1_TwoCircles/cmake-build-debug/CMakeFiles/1_TwoCircles.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1_TwoCircles.dir/main.cpp.o"
+  "1_TwoCircles.pdb"
+  "1_TwoCircles.exe"
+  "lib1_TwoCircles.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_TwoCircles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
